67603b7dc75778f42a05aa16f402b77f765c20f2
ajgeiss0702 <ajgeiss72@gmail.com>
2021-11-24 13:41:31 -0700

    more progress

--- src/main/java/us/ajg0702/leaderboards/LeaderboardPlugin.java
+++ src/main/java/us/ajg0702/leaderboards/LeaderboardPlugin.java
@@ -1,5 +1,10 @@
 package us.ajg0702.leaderboards;
 
+import net.milkbowl.vault.chat.Chat;
+import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.configurate.ConfigurateException;
@@ -20,12 +25,18 @@ public class LeaderboardPlugin extends JavaPlugin {
     private Messages messages;
     private TopManager topManager;
 
+    private boolean vault;
+    private Chat vaultChat;
+
     @Override
     public void onEnable() {
 
         BukkitCommand bukkitMainCommand = new BukkitCommand(new MainCommand(this));
-        getCommand("ajleaderboards").setExecutor(bukkitMainCommand);
-        getCommand("ajleaderboards").setTabCompleter(bukkitMainCommand);
+
+        PluginCommand mainCommand = getCommand("ajleaderboards");
+        assert mainCommand != null;
+        mainCommand.setExecutor(bukkitMainCommand);
+        mainCommand.setTabCompleter(bukkitMainCommand);
 
         BukkitSender.setAdventure(this);
 
@@ -52,8 +63,27 @@ public class LeaderboardPlugin extends JavaPlugin {
         dmsgs.put("formatted.b", "b");
         dmsgs.put("formatted.q", "q");
 
+        dmsgs.put("noperm", "You don't have permission to do this!");
+
+        dmsgs.put("commands.reload.success", "&aConfigs reloaded!");
+        dmsgs.put("commands.reload.fail", "&cAn error occurred while reloading one of your configs. Check the console for more info.");
 
         messages = new Messages(getDataFolder(), getLogger(), dmsgs);
+
+        Bukkit.getScheduler().runTask(this, () -> {
+            if(Bukkit.getPluginManager().isPluginEnabled("Vault")) {
+                RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+                if(rsp == null) {
+                    vault = false;
+                    getLogger().warning("Vault prefix hook failed! Make sure you have a plugin that implements chat (e.g. Luckperms)");
+                } else {
+                    vaultChat = rsp.getProvider();
+                    vault = vaultChat != null;
+                }
+            }
+        });
+
+        reloadInterval();
 
         getLogger().info("ajLeaderboards v"+getDescription().getVersion()+" by ajgeiss0702 enabled!");
     }
@@ -77,5 +107,28 @@ public class LeaderboardPlugin extends JavaPlugin {
 
     public Messages getMessages() {
         return messages;
+    }
+
+    public boolean hasVault() {
+        return vault;
+    }
+
+    public Chat getVaultChat() {
+        return vaultChat;
+    }
+
+    int updateTaskId = -1;
+    public void reloadInterval() {
+        if(updateTaskId != -1) {
+            try {
+                Bukkit.getScheduler().cancelTask(updateTaskId);
+            } catch(IllegalArgumentException ignored) {}
+            updateTaskId = -1;
+        }
+        updateTaskId = Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> {
+            for(Player p : Bukkit.getOnlinePlayers()) {
+                getCache().updatePlayerStats(p);
+            }
+        }, 10*20, config.getInt("stat-refresh")).getTaskId();
     }
 }
