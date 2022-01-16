f74c7c6df698e548502eec52e0cf79f9f4955457
ajgeiss0702 <ajgeiss72@gmail.com>
2022-01-16 10:05:27 -0700

    cancel tasks on shutdown

--- src/main/java/us/ajg0702/leaderboards/LeaderboardPlugin.java
+++ src/main/java/us/ajg0702/leaderboards/LeaderboardPlugin.java
@@ -133,6 +133,7 @@ public class LeaderboardPlugin extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        Bukkit.getScheduler().cancelTasks(this);
         getCache().getMethod().shutdown();
         getLogger().info("ajLeaderboards v"+getDescription().getVersion()+" disabled.");
     }
