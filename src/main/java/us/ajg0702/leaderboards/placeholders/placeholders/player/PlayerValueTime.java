17955622b6e495d15910971a6e581128466baa4e
ajgeiss0702 <ajgeiss72@gmail.com>
2022-01-23 22:26:00 -0700

    Added %ajlb_value_<board>_<type>_time% and %ajlb_value_<board>_<type>_raw%

---
+++ src/main/java/us/ajg0702/leaderboards/placeholders/placeholders/player/PlayerValueTime.java
@@ -0,0 +1,27 @@
+package us.ajg0702.leaderboards.placeholders.placeholders.player;
+
+import org.bukkit.OfflinePlayer;
+import us.ajg0702.leaderboards.LeaderboardPlugin;
+import us.ajg0702.leaderboards.boards.TimedType;
+import us.ajg0702.leaderboards.placeholders.Placeholder;
+
+import java.util.Locale;
+import java.util.regex.Matcher;
+
+public class PlayerValueTime extends Placeholder {
+    public PlayerValueTime(LeaderboardPlugin plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public String getRegex() {
+        return "value_(*.*)_(*.*)_time";
+    }
+
+    @Override
+    public String parse(Matcher matcher, OfflinePlayer p) {
+        String board = matcher.group(1);
+        String typeRaw = matcher.group(2).toUpperCase(Locale.ROOT);
+        return plugin.getTopManager().getStatEntry(p, board, TimedType.valueOf(typeRaw)).getTime();
+    }
+}
