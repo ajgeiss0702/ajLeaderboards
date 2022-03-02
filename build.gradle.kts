5d1d0de10c006355105f41def68f2d580d8c75fc
ajgeiss0702 <ajgeiss72@gmail.com>
2022-03-02 12:52:47 -0700

    auto replace legacy color codes with adventure tags

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("net.kyori:adventure-text-minimessage:4.10.0")
     implementation("net.kyori:adventure-platform-bukkit:4.0.1")
 
-    implementation("us.ajg0702:ajUtils:1.1.19")
+    implementation("us.ajg0702:ajUtils:1.1.20")
     implementation("us.ajg0702.commands.platforms.bukkit:bukkit:1.0.0-pre14")
     implementation("us.ajg0702.commands.api:api:1.0.0-pre14")
 
