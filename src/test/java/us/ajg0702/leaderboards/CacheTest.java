278f77e18897fabda272cbad1b779813fca18e8d
ajgeiss0702 <ajgeiss72@gmail.com>
2022-04-11 08:49:41 -0700

    fix test

--- src/test/java/us/ajg0702/leaderboards/CacheTest.java
+++ src/test/java/us/ajg0702/leaderboards/CacheTest.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class CacheTest {
     @Test
     public void testDecimal() throws Exception {
-        StatEntry se = new StatEntry(null, 1, "test_board", "testprefix", "testplayer", UUID.randomUUID(), "testsuffix", 1.5, TimedType.ALLTIME);
+        StatEntry se = new StatEntry(null, 1, "test_board", "testprefix", "testplayer", "testplayerdisplay", UUID.randomUUID(), "testsuffix", 1.5, TimedType.ALLTIME);
         //System.out.println("score pretty 1.5: "+se.getScorePretty());
         if(!se.getScorePretty().equals("1.5")) {
             throw new Exception("Score is "+se.getScorePretty()+" instead of 1.5");
@@ -19,7 +19,7 @@ public class CacheTest {
     }
     @Test
     public void testComma() throws Exception {
-        StatEntry se = new StatEntry(null, 1, "test_board", "testprefix", "testplayer", UUID.randomUUID(), "testsuffix", 1500, TimedType.ALLTIME);
+        StatEntry se = new StatEntry(null, 1, "test_board", "testprefix", "testplayer", "testplayerdisplay", UUID.randomUUID(), "testsuffix", 1500, TimedType.ALLTIME);
         if(!se.getScorePretty().equals("1,500")) {
             throw new Exception("Score is "+se.getScorePretty()+" instead of 1,500");
         }
@@ -27,7 +27,7 @@ public class CacheTest {
     }
     @Test
     public void testCommaDecimal() throws Exception {
-        StatEntry se = new StatEntry(null, 1, "test_board", "testprefix", "testplayer", UUID.randomUUID(), "testsuffix", 1500.5, TimedType.ALLTIME);
+        StatEntry se = new StatEntry(null, 1, "test_board", "testprefix", "testplayer", "testplayerdisplay", UUID.randomUUID(), "testsuffix", 1500.5, TimedType.ALLTIME);
         if(!se.getScorePretty().equals("1,500.5")) {
             throw new Exception("Score is "+se.getScorePretty()+" instead of 1,500.5");
         }
