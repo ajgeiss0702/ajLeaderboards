f6e9c7d78eede3ec5864b5cc5afa176d63069b18
ajgeiss0702 <ajgeiss72@gmail.com>
2022-09-27 17:59:59 -0700

    [nolist] bandaid fix for zeroPlayers memory leak

--- src/main/java/us/ajg0702/leaderboards/Listeners.java
+++ src/main/java/us/ajg0702/leaderboards/Listeners.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class Listeners implements Listener {
 
@@ -18,5 +19,10 @@ public class Listeners implements Listener {
         if(!plugin.getAConfig().getBoolean("update-stats")) return;
         if(!plugin.getAConfig().getBoolean("update-on-join")) return;
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> plugin.getCache().updatePlayerStats(e.getPlayer()));
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        plugin.getCache().cleanPlayer(e.getPlayer());
     }
 }
