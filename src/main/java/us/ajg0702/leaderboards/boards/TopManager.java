1323560da36ab9bcb07a3a43674f66c3398a7e39
Marcel <8513034+MrEAlderson@users.noreply.github.com>
2022-10-01 20:26:35 +0200

    Fixed exaggerated threads handling

--- src/main/java/us/ajg0702/leaderboards/boards/TopManager.java
+++ src/main/java/us/ajg0702/leaderboards/boards/TopManager.java
@@ -42,9 +42,9 @@ public class TopManager {
         CacheMethod method = plugin.getCache().getMethod();
         int t = method instanceof MysqlMethod ? Math.max(10, method.getMaxConnections()) : plugin.getAConfig().getInt("max-fetching-threads");
         fetchService = new ThreadPoolExecutor(
-                t, t,
-                500, TimeUnit.MILLISECONDS,
-                new ArrayBlockingQueue<>(1000000, true)
+                1, t,
+                60, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>()
         );
         fetchService.allowCoreThreadTimeOut(true);
         fetchService.setThreadFactory(ThreadFactoryProxy.getDefaultThreadFactory("AJLBFETCH"));
