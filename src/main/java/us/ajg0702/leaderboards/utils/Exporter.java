fe25572230b94d720cfd4c82c70d9f9dfe7b2646
ajgeiss0702 <ajgeiss72@gmail.com>
2023-01-01 21:55:31 -0700

    Add metadata to exports

--- src/main/java/us/ajg0702/leaderboards/utils/Exporter.java
+++ src/main/java/us/ajg0702/leaderboards/utils/Exporter.java
@@ -1,9 +1,12 @@
 package us.ajg0702.leaderboards.utils;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import net.kyori.adventure.audience.Audience;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
+import us.ajg0702.commands.CommandSender;
 import us.ajg0702.leaderboards.LeaderboardPlugin;
 import us.ajg0702.leaderboards.cache.helpers.DbRow;
 
@@ -22,6 +25,13 @@ public class Exporter {
 
     public JsonObject export(@Nullable Audience reportTo) {
         if(reportTo != null) reportTo.sendMessage(plugin.getMessages().getComponent("commands.export.starting"));
+        String takerUUID = "f78a4d8d-d51b-4b39-98a3-230f2de0c670";
+        String takerName = "Console";
+        if(reportTo instanceof CommandSender && ((CommandSender) reportTo).isPlayer()) {
+            Player taker = (Player) ((CommandSender) reportTo).getHandle();
+            takerUUID = taker.getUniqueId().toString();
+            takerName = taker.getName();
+        }
         plugin.getLogger().info("Starting export");
 
         HashMap<String, List<DbRow>> rows = new HashMap<>();
@@ -45,6 +55,18 @@ public class Exporter {
             entry.getValue().forEach((t) -> elements.add(t.toJsonObject()));
             obj.add(entry.getKey(), elements);
         }
+
+        obj.add("meta", new Gson().fromJson(
+                "{" +
+                        "\"version\": \"" + plugin.getDescription().getVersion() + "\", " +
+                        "\"datestamp\": " + System.currentTimeMillis() + ", " +
+                        "\"taker\": {" +
+                        "\"uuid\": \"" + takerUUID +"\"," +
+                        "\"name\": \"" + takerName + "\"" +
+                        "}" +
+                        "}",
+                JsonObject.class
+        ));
 
         return obj;
     }
