ee8bc8fadcb3a4d1973a580bed3a5fbecf2154ce
ajgeiss0702 <ajgeiss72@gmail.com>
2024-01-26 11:32:10 -0700

    update adventure-platform-bukkit

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
 
     implementation("net.kyori:adventure-api:4.12.0")
     implementation("net.kyori:adventure-text-minimessage:4.12.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.3.0")
+    implementation("net.kyori:adventure-platform-bukkit:4.3.2")
 
     implementation("us.ajg0702:ajUtils:1.2.24")
     implementation("us.ajg0702.commands.platforms.bukkit:bukkit:1.0.0")
