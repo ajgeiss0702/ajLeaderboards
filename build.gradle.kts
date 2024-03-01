525e957c1235f242f834058deebe57c45e59bd17
ajgeiss0702 <ajgeiss72@gmail.com>
2024-03-01 12:16:13 -0700

    Update ajUtils

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("net.kyori:adventure-text-minimessage:4.12.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.2")
 
-    implementation("us.ajg0702:ajUtils:1.2.24")
+    implementation("us.ajg0702:ajUtils:1.2.25")
     implementation("us.ajg0702.commands.platforms.bukkit:bukkit:1.0.0")
     implementation("us.ajg0702.commands.api:api:1.0.0")
 
