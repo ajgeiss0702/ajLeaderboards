33f3a6520f55cca405a50f242535987d8aa19544
ajgeiss0702 <aj@ajg0702.us>
2024-07-08 16:38:50 -0700

    add some plan time placeholders to time placeholder list

--- src/main/java/us/ajg0702/leaderboards/formatting/formats/Time.java
+++ src/main/java/us/ajg0702/leaderboards/formatting/formats/Time.java
@@ -41,7 +41,9 @@ public class Time extends Format {
             "statistic_time_played",
             "statistic_time_since_death",
             "mbedwars_stats-play_time",
-            "formatter_number_time_*"
+            "formatter_number_time_*",
+            "plan_player_time_active",
+            "plan_player_time_afk"
     );
 
     private boolean isKnownTimePlaceholder(String placeholder) {
